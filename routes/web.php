1204a02e6a92d59ad84eb2bf4774a325b080d524
Minos32x <minasamy65@gmail.com>
2018-04-21 00:18:46 +0200

    Add Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function(){
+
+    return view('Admin.admin_template');
+});
