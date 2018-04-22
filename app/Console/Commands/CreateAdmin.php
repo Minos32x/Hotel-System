6f842686468079b9251fd23f4e5288ecf8554ae1
Minos32x <minasamy65@gmail.com>
2018-04-22 20:29:00 +0200

    Add CreateAdmin Function Through CLI

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:admin {--name=} {--password=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It Will Create New Admin User';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        public function handle()
+    {
+        DB::table('users')->insert([
+            'email' => $this->option('name'),
+            'password' => $this->option('password'),
+        ]);
+
+        $this->error('Error Missing Arguments');
+
+    }
+    }
+}
