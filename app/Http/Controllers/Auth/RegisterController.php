90ee9c537cb027fbd91e42b28f288ff98be7ba5d
minaibrahimsaid <minaibrahim1991@yahoo.com>
2018-04-23 15:35:41 +0200

    add register form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'phone'=>$data['phone'],
+            'country'=>'Egypt',
+            'last_login'=>now(),
         ]);
     }
 }
