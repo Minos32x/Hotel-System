9dfcef8dc2c33954b178b01729aa1572ff7db843
ahmedkamel1192 <ahmed.kamel1192@gmail.com>
2018-04-27 22:58:06 +0200

    modify in edit page of client

--- resources/views/Admin/btn/client_action.blade.php
+++ resources/views/Admin/btn/client_action.blade.php
@@ -1,6 +1,6 @@
 
 <?php if (Auth::guard('employee')->user()->id === $model->approved_by  || (Auth::guard('employee')->user()->type === 'admin')|| (Auth::guard('employee')->user()->type === 'manager')) { ?>
-<a href ="{{ url('client/'.$id.'/edit') }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
+<a href ="{{ url('clients/'.$id.'/edit') }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
 <form action="{{ url('employees/'.$id) }}" method="post" style="display: inline-block">
 {{ csrf_field()}}
 {{ method_field('DELETE')}}
