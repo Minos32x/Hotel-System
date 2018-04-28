b43817eca1bc131a8736d80549466615aa536302
F-Yousri <fahdyousri@gmail.com>
2018-04-28 20:04:49 +0200

    fix bug on clientsDataTable

--- app/Http/Controllers/ClientsViewsController.php
+++ app/Http/Controllers/ClientsViewsController.php
@@ -96,9 +96,7 @@ class ClientsViewsController extends Controller
     {
 
         if (isset($_POST['stripeToken'])) {
-
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
+// Set your secret key: remember to change this to your live secret key in production See your keys here: https://dashboard.stripe.com/account/apikeys
             \Stripe\Stripe::setApiKey("sk_test_85Rd2lxhCpyYqchbpvfgEi1u");
             $customer = \Stripe\Customer::create([
                 'source' => 'tok_mastercard',
